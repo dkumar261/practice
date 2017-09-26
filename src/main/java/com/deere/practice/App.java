fb638d05e685588046e6742b242760861340478f
dinesh <dinesh@dinesh-VAIO>
2017-09-26 14:30:11 -0700

    first commit

---
+++ src/main/java/com/deere/practice/App.java
@@ -0,0 +1,30 @@
+package com.deere.practice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.deere.practice.config.BeanConfiguration;
+import com.deere.practice.greeting.Diwali;
+import com.deere.practice.greeting.TopGreeting;
+
+/**
+ * Hello world! String
+ * 
+ */
+
+@SpringBootApplication
+public class App  {
+	
+	@Autowired
+	private TopGreeting greeting ;
+	
+	public static void main(String[] args) {
+		ApplicationContext context= new AnnotationConfigApplicationContext(BeanConfiguration.class);
+		Diwali d = context.getBean("diwali",Diwali.class);
+		System.out.println(d.say());
+//		SpringApplication.run(App.class, args);
+	}
+	
+}
